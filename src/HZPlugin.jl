2708eb682c457883a9d4827d919408e93fc3e69a
Talom Laël <gesee37@gmail.com>
2025-11-12 10:55:41 +0100

    Update HZPlugin.jl

--- src/HZPlugin.jl
+++ src/HZPlugin.jl
@@ -4,7 +4,7 @@
 
 module HZPlugin
 
-export HZPLUGIN, RegisterBackend
+export HZPLUGIN, RegisterBackend, GetBackend, CreateBackend
 
 using Reexport
 using Cruise
@@ -30,9 +30,9 @@ end
 
 const HZPLUGIN = CRPlugin()
 const MANAGER = HorizonManager()
-PHASE = :postupdate
+PHASE = :preupdate
 
-const ID = add_system!(HZPLUGIN, MANAGER)
+const ID = add_system!(HZPLUGIN, MANAGER; mainthread=true)
 
 Horizons.connect(HORIZON_ERROR) do msg,err
 	node = HZPLUGIN.idtonode[ID]
@@ -42,10 +42,25 @@ end
 
 ################################################# PLUGIN LIFECYCLE ####################################################
 
+function GetBackend(win)
+	for (k,v) in MANAGER.backends
+		v.value == win && return k
+	end
+end
+
+function CreateBackend(R::Type{<:HRenderer}, win, sizex, sizey, x=0, y=0)
+	backend = InitBackend(R, win)
+	CreateViewport(backend, sizex, sizey, x, y)
+	MANAGER.backends[backend] = WeakRef(win)
+
+	return backend
+end
+
 function Cruise.awake!(n::CRPluginNode{HorizonManager})
 	for (R, data) in MANAGER.data
 		win = data.win
-		backend = InitBackend(R, win, data.args...)
+		backend = InitBackend(R, win)
+		CreateViewport(backend, data.args...)
 		MANAGER.backends[backend] = WeakRef(win)
 	end
 
@@ -53,14 +68,15 @@ function Cruise.awake!(n::CRPluginNode{HorizonManager})
 	setstatus(n, PLUGIN_OK)
 end
 
-function Cruise.update!(n::CRPluginNode{HorizonManager}, dt)
+function Cruise.update!(n::CRPluginNode{HorizonManager})
 	manager = n.obj
 	backends = keys(manager.backends)
+
 	for (backend, winref) in manager.backends
 		if isnothing(winref.value)
 			# Given that there are a small number of renderers in a game,
 			# No need to move memory to delete the deprecated backend, we can just skip them
-			continue
+			#continue
 		end
         SetDrawColor(backend,WHITE)
         ClearViewport(backend)
@@ -77,3 +93,5 @@ end
 function RegisterBackend(R::Type, win, args...)
 	MANAGER.data[R] = HZRegistration(win,args)
 end
+
+end # module
